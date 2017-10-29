3b3f4bc55a0ef1fe33411995f0382e9a39621175
Your Name <you@example.com>
2017-10-29 20:37:04 +0100

    in the car modified:   codewar.py

--- codewar.py
+++ codewar.py
@@ -1,30 +1,38 @@
-import curses
-from curses import wrapper
 
+import curses
+from curses import wrapper,KEY_RIGHT,KEY_LEFT,KEY_UP,KEY_DOWN            
 def main(screen):
     
-    while True:
-        # Clear screen
-        screen.clear()
-        screen.refresh()
-        curses.cbreak()
-        world = curses.newpad(500,500)
-        y,x = 100,100
-        h,w = screen.getmaxyx()
-        #world.addstr("hello"*(500*500))
-        for y in range(0, 500):
-            for x in range(0, 500):
-                try:
-                    world.addch(y,x, ord('a') + (x*x+y*y) % 26)
-                except curses.error:
-                    pass
-
-        while 1:
+    
+    # Clear screen
+    curses.noecho()
+    curses.cbreak()
+    screen.keypad(1) 
+    screen.refresh()
+    screen.border(0)
+    
+    world = curses.newpad(500,500)
+    y,x = 0,0
+    h,w = screen.getmaxyx()
+    world.addstr("hello"*(499))
+    world.move(10,10)
+    cursor = (10,10)
+    world.refresh(x,y,1,1,h-2,w-2)
+    world.keypad(1)
+    while 1:
             
-            c = world.getkey()
-            if c == curses.KEY_BACKSPACE:
-                break
-            world.refresh(x,y,0,0,h-1,w-1)
-        #stdscr.getkey()           
+        key = world.getch()
+        if key == ord('q'):
+            break
+        cursor = world.getyx()  
+        world.move(cursor[0]+ 
+            (key == KEY_DOWN and 1)+ 
+            (key == KEY_UP and -1), 
+           	cursor[1] +
+           	 (key == KEY_LEFT and -1)+
+             (key == KEY_RIGHT and 1))
+        
+        world.refresh(x,y,1,1,h-2,w-2)
+        #stdscr.getkey()                   
 
 wrapper(main)
